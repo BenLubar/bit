ae91d396ad73ed9d508f864f335d105520345e53
Ben Lubar <ben.lubar@gmail.com>
2015-08-23 18:38:21 -0500

    add an AST form of basic.cool with placeholders for native functions

--- cmd/coolc/semantic.go
+++ cmd/coolc/semantic.go
@@ -1,5 +1,1097 @@
 package main
 
+import "fmt"
+
+var extendsAny = &ExtendsDecl{
+	Type: TYPE{
+		Name:   "Any",
+		target: basicAny,
+	},
+}
+
+var basicAny = &ClassDecl{
+	Name: TYPE{
+		Name: "Any",
+	},
+	Body: []Feature{
+		&MethodFeature{
+			Name: ID{
+				Name: "toString",
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "equals",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "x",
+					},
+					Type: TYPE{
+						Name: "Any",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Boolean",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+	},
+}
+
+var basicIO = &ClassDecl{
+	Name: TYPE{
+		Name: "IO",
+	},
+	Body: []Feature{
+		&MethodFeature{
+			Name: ID{
+				Name: "abort",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "message",
+					},
+					Type: TYPE{
+						Name: "String",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Nothing",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "out",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "arg",
+					},
+					Type: TYPE{
+						Name: "String",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "IO",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "is_null",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "arg",
+					},
+					Type: TYPE{
+						Name: "Any",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Boolean",
+			},
+			Body: &MatchExpr{
+				Left: &NameExpr{
+					Name: ID{
+						Name: "arg",
+					},
+				},
+				Cases: &Cases{
+					Cases: []*Case{
+						{
+							Name: ID{
+								Name: "x",
+							},
+							Type: TYPE{
+								Name: "Any",
+							},
+							Body: &BooleanExpr{
+								B: false,
+							},
+						},
+					},
+					Null: &BooleanExpr{
+						B: true,
+					},
+				},
+			},
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "out_any",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "arg",
+					},
+					Type: TYPE{
+						Name: "Any",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "IO",
+			},
+			Body: &SelfCallExpr{
+				Name: ID{
+					Name: "out",
+				},
+				Args: []Expr{
+					&IfExpr{
+						Condition: &SelfCallExpr{
+							Name: ID{
+								Name: "is_null",
+							},
+							Args: []Expr{
+								&NameExpr{
+									Name: ID{
+										Name: "arg",
+									},
+								},
+							},
+						},
+						Then: &StringExpr{
+							S: "null",
+						},
+						Else: &CallExpr{
+							Left: &NameExpr{
+								Name: ID{
+									Name: "arg",
+								},
+							},
+							Name: ID{
+								Name: "toString",
+							},
+						},
+					},
+				},
+			},
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "in",
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "symbol",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "name",
+					},
+					Type: TYPE{
+						Name: "String",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Symbol",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "symbol_name",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "sym",
+					},
+					Type: TYPE{
+						Name: "Symbol",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+	},
+}
+
+var basicUnit = &ClassDecl{
+	Name: TYPE{
+		Name: "Unit",
+	},
+	Body: []Feature{
+		&NativeFeature{},
+	},
+}
+
+var basicInt = &ClassDecl{
+	Name: TYPE{
+		Name: "Int",
+	},
+	Body: []Feature{
+		&NativeFeature{},
+		&MethodFeature{
+			Override: true,
+			Name: ID{
+				Name: "toString",
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Override: true,
+			Name: ID{
+				Name: "equals",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "other",
+					},
+					Type: TYPE{
+						Name: "Any",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Boolean",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+	},
+}
+
+var basicBoolean = &ClassDecl{
+	Name: TYPE{
+		Name: "Boolean",
+	},
+	Body: []Feature{
+		&NativeFeature{},
+		&MethodFeature{
+			Override: true,
+			Name: ID{
+				Name: "toString",
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: &IfExpr{
+				Condition: &ThisExpr{},
+				Then: &StringExpr{
+					S: "true",
+				},
+				Else: &StringExpr{
+					S: "false",
+				},
+			},
+		},
+		&MethodFeature{
+			Override: true,
+			Name: ID{
+				Name: "equals",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "other",
+					},
+					Type: TYPE{
+						Name: "Any",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Boolean",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+	},
+}
+
+var basicString = &ClassDecl{
+	Name: TYPE{
+		Name: "String",
+	},
+	Body: []Feature{
+		&VarFeature{
+			VarDecl: VarDecl{
+				Name: ID{
+					Name: "length",
+				},
+				Type: TYPE{
+					Name: "Int",
+				},
+			},
+			Value: &IntegerExpr{
+				N: 0,
+			},
+		},
+		&NativeFeature{},
+		&MethodFeature{
+			Override: true,
+			Name: ID{
+				Name: "toString",
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: &ThisExpr{},
+		},
+		&MethodFeature{
+			Override: true,
+			Name: ID{
+				Name: "equals",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "other",
+					},
+					Type: TYPE{
+						Name: "Any",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Boolean",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "length",
+			},
+			Return: TYPE{
+				Name: "Int",
+			},
+			Body: &NameExpr{
+				Name: ID{
+					Name: "length",
+				},
+			},
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "concat",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "arg",
+					},
+					Type: TYPE{
+						Name: "String",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "substring",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "start",
+					},
+					Type: TYPE{
+						Name: "Int",
+					},
+				},
+				{
+					Name: ID{
+						Name: "end",
+					},
+					Type: TYPE{
+						Name: "Int",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "charAt",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "index",
+					},
+					Type: TYPE{
+						Name: "Int",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Int",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "indexOf",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "sub",
+					},
+					Type: TYPE{
+						Name: "String",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Int",
+			},
+			Body: &VarExpr{
+				VarFeature: VarFeature{
+					VarDecl: VarDecl{
+						Name: ID{
+							Name: "n",
+						},
+						Type: TYPE{
+							Name: "Int",
+						},
+					},
+					Value: &CallExpr{
+						Left: &NameExpr{
+							Name: ID{
+								Name: "sub",
+							},
+						},
+						Name: ID{
+							Name: "length",
+						},
+					},
+				},
+				Expr: &VarExpr{
+					VarFeature: VarFeature{
+						VarDecl: VarDecl{
+							Name: ID{
+								Name: "diff",
+							},
+							Type: TYPE{
+								Name: "Int",
+							},
+						},
+						Value: &SubtractExpr{
+							Left: &NameExpr{
+								Name: ID{
+									Name: "length",
+								},
+							},
+							Right: &NameExpr{
+								Name: ID{
+									Name: "n",
+								},
+							},
+						},
+					},
+					Expr: &VarExpr{
+						VarFeature: VarFeature{
+							VarDecl: VarDecl{
+								Name: ID{
+									Name: "i",
+								},
+								Type: TYPE{
+									Name: "Int",
+								},
+							},
+							Value: &IntegerExpr{
+								N: 0,
+							},
+						},
+						Expr: &VarExpr{
+							VarFeature: VarFeature{
+								VarDecl: VarDecl{
+									Name: ID{
+										Name: "result",
+									},
+									Type: TYPE{
+										Name: "Int",
+									},
+								},
+								Value: &NegativeExpr{
+									Right: &IntegerExpr{
+										N: 1,
+									},
+								},
+							},
+							Expr: &ChainExpr{
+								Pre: &WhileExpr{
+									Condition: &LessThanOrEqualExpr{
+										Left: &NameExpr{
+											Name: ID{
+												Name: "i",
+											},
+										},
+										Right: &NameExpr{
+											Name: ID{
+												Name: "diff",
+											},
+										},
+									},
+									Do: &IfExpr{
+										Condition: &CallExpr{
+											Left: &SelfCallExpr{
+												Name: ID{
+													Name: "substring",
+												},
+												Args: []Expr{
+													&NameExpr{
+														Name: ID{
+															Name: "i",
+														},
+													},
+													&AddExpr{
+														Left: &NameExpr{
+															Name: ID{
+																Name: "i",
+															},
+														},
+														Right: &NameExpr{
+															Name: ID{
+																Name: "n",
+															},
+														},
+													},
+												},
+											},
+											Name: ID{
+												Name: "equals",
+											},
+											Args: []Expr{
+												&NameExpr{
+													Name: ID{
+														Name: "sub",
+													},
+												},
+											},
+										},
+										Then: &ChainExpr{
+											Pre: &AssignExpr{
+												Left: ID{
+													Name: "result",
+												},
+												Right: &NameExpr{
+													Name: ID{
+														Name: "i",
+													},
+												},
+											},
+											Expr: &AssignExpr{
+												Left: ID{
+													Name: "i",
+												},
+												Right: &AddExpr{
+													Left: &NameExpr{
+														Name: ID{
+															Name: "diff",
+														},
+													},
+													Right: &IntegerExpr{
+														N: 1,
+													},
+												},
+											},
+										},
+										Else: &AssignExpr{
+											Left: ID{
+												Name: "i",
+											},
+											Right: &AddExpr{
+												Left: &NameExpr{
+													Name: ID{
+														Name: "i",
+													},
+												},
+												Right: &IntegerExpr{
+													N: 1,
+												},
+											},
+										},
+									},
+								},
+								Expr: &NameExpr{
+									Name: ID{
+										Name: "result",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
+var basicSymbol = &ClassDecl{
+	Name: TYPE{
+		Name: "Symbol",
+	},
+	Body: []Feature{
+		&VarFeature{
+			VarDecl: VarDecl{
+				Name: ID{
+					Name: "name",
+				},
+				Type: TYPE{
+					Name: "String",
+				},
+			},
+			Value: &StringExpr{
+				S: "",
+			},
+		},
+		&VarFeature{
+			VarDecl: VarDecl{
+				Name: ID{
+					Name: "hash",
+				},
+				Type: TYPE{
+					Name: "Int",
+				},
+			},
+			Value: &IntegerExpr{
+				N: 0,
+			},
+		},
+		&NativeFeature{},
+		&MethodFeature{
+			Override: true,
+			Name: ID{
+				Name: "toString",
+			},
+			Return: TYPE{
+				Name: "String",
+			},
+			Body: &CallExpr{
+				Left: &StringExpr{
+					S: "'",
+				},
+				Name: ID{
+					Name: "concat",
+				},
+				Args: []Expr{
+					&NameExpr{
+						Name: ID{
+							Name: "name",
+						},
+					},
+				},
+			},
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "hashCode",
+			},
+			Return: TYPE{
+				Name: "Int",
+			},
+			Body: &NameExpr{
+				Name: ID{
+					Name: "hash",
+				},
+			},
+		},
+	},
+}
+
+var basicArrayAny = &ClassDecl{
+	Name: TYPE{
+		Name: "ArrayAny",
+	},
+	Args: []*VarDecl{
+		{
+			Name: ID{
+				Name: "length",
+			},
+			Type: TYPE{
+				Name: "Int",
+			},
+		},
+	},
+	Body: []Feature{
+		&NativeFeature{},
+		&MethodFeature{
+			Name: ID{
+				Name: "length",
+			},
+			Return: TYPE{
+				Name: "Int",
+			},
+			Body: &NameExpr{
+				Name: ID{
+					Name: "length",
+				},
+			},
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "resize",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "s",
+					},
+					Type: TYPE{
+						Name: "Int",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "ArrayAny",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "get",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "index",
+					},
+					Type: TYPE{
+						Name: "Int",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Any",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+		&MethodFeature{
+			Name: ID{
+				Name: "set",
+			},
+			Args: []*VarDecl{
+				{
+					Name: ID{
+						Name: "index",
+					},
+					Type: TYPE{
+						Name: "Int",
+					},
+				},
+				{
+					Name: ID{
+						Name: "obj",
+					},
+					Type: TYPE{
+						Name: "Any",
+					},
+				},
+			},
+			Return: TYPE{
+				Name: "Any",
+			},
+			Body: NativeExpr(func(w *writer) error {
+				panic("unimplemented")
+			}),
+		},
+	},
+}
+
+var basicClasses = []*ClassDecl{
+	basicAny,
+	basicIO,
+	basicUnit,
+	basicInt,
+	basicBoolean,
+	basicString,
+	basicSymbol,
+	basicArrayAny,
+}
+
+func init() {
+	basicAST := &AST{
+		Classes: append([]*ClassDecl{
+			&ClassDecl{
+				Name: TYPE{
+					Name: "Main",
+				},
+			},
+		}, basicClasses...),
+	}
+	if err := basicAST.Semantic(); err != nil {
+		panic(err)
+	}
+}
+
 func (ast *AST) Semantic() (err error) {
-	panic("unimplemented")
+	classes := make(map[string]*ClassDecl)
+
+	for _, c := range basicClasses {
+		classes[c.Name.Name] = c
+	}
+
+	for _, c := range ast.Classes {
+		if c.Name.Name == "Nothing" || c.Name.Name == "Null" {
+			cp := ast.FileSet.Position(c.Name.Pos)
+			return fmt.Errorf("cannot define a class with name %s at %v", c.Name.Name, cp)
+		}
+		if o, ok := classes[c.Name.Name]; ok && c != o {
+			op := ast.FileSet.Position(o.Name.Pos)
+			cp := ast.FileSet.Position(c.Name.Pos)
+			return fmt.Errorf("multiple classes with name %s at %v, %v", c.Name.Name, op, cp)
+		}
+		classes[c.Name.Name] = c
+		if c.Extends == nil {
+			c.Extends = extendsAny
+		}
+		if c.Name.Name == "Main" {
+			ast.main = c
+		}
+	}
+
+	if ast.main == nil {
+		return fmt.Errorf("missing Main class")
+	}
+
+	var recurse func([]*ID, interface{})
+	recurse = func(ns []*ID, value interface{}) {
+		addNS := func(target interface{}, name *ID) {
+			for _, n := range ns {
+				if n.Name == name.Name {
+					nPos := ast.FileSet.Position(n.Pos)
+					namePos := ast.FileSet.Position(name.Pos)
+					panic(fmt.Errorf("shadowing is not allowed (%s) at %v, %v", name.Name, nPos, namePos))
+				}
+			}
+			ns = append(ns, name)
+			name.target = target
+		}
+		switch v := value.(type) {
+		case *ID:
+			for _, n := range ns {
+				if n.Name == v.Name {
+					v.target = n.target
+					return
+				}
+			}
+			pos := ast.FileSet.Position(v.Pos)
+			panic(fmt.Errorf("undeclared identifier (%s) at %v", v.Name, pos))
+
+		case *TYPE:
+			if v.Name == "Nothing" || v.Name == "Null" {
+				return
+			}
+			if c, ok := classes[v.Name]; ok {
+				v.target = c
+				return
+			}
+			pos := ast.FileSet.Position(v.Pos)
+			panic(fmt.Errorf("undeclared type (%s) at %v", v.Name, pos))
+
+		case *ClassDecl:
+			recurse(ns, &v.Name)
+			for _, a := range v.Args {
+				addNS(a, &a.Name)
+				recurse(ns, &a.Type)
+			}
+			recurse(ns, v.Extends)
+			for _, f := range v.Body {
+				if a, ok := f.(*VarFeature); ok {
+					addNS(a, &a.Name)
+				}
+			}
+			for _, f := range v.Body {
+				recurse(ns, f)
+			}
+
+		case *ExtendsDecl:
+			recurse(ns, &v.Type)
+			for _, e := range v.Args {
+				recurse(ns, e)
+			}
+
+		case *VarFeature:
+			recurse(ns, &v.Type)
+			recurse(ns, v.Value)
+
+		case *MethodFeature:
+			recurse(ns, &v.Return)
+			for _, a := range v.Args {
+				addNS(a, &a.Name)
+				recurse(ns, &a.Type)
+			}
+			recurse(ns, v.Body)
+
+		case *BlockFeature:
+			recurse(ns, v.Expr)
+
+		case *NativeFeature:
+
+		case *SelfCallExpr:
+			for _, a := range v.Args {
+				recurse(ns, a)
+			}
+
+		case *CallExpr:
+			recurse(ns, v.Left)
+			for _, a := range v.Args {
+				recurse(ns, a)
+			}
+
+		case *NegativeExpr:
+			recurse(ns, v.Right)
+
+		case *NotExpr:
+			recurse(ns, v.Right)
+
+		case *AddExpr:
+			recurse(ns, v.Left)
+			recurse(ns, v.Right)
+
+		case *SubtractExpr:
+			recurse(ns, v.Left)
+			recurse(ns, v.Right)
+
+		case *MultiplyExpr:
+			recurse(ns, v.Left)
+			recurse(ns, v.Right)
+
+		case *DivideExpr:
+			recurse(ns, v.Left)
+			recurse(ns, v.Right)
+
+		case *LessThanExpr:
+			recurse(ns, v.Left)
+			recurse(ns, v.Right)
+
+		case *LessThanOrEqualExpr:
+			recurse(ns, v.Left)
+			recurse(ns, v.Right)
+
+		case *NameExpr:
+			recurse(ns, &v.Name)
+
+		case *ThisExpr:
+
+		case *StringExpr, *IntegerExpr, *BooleanExpr:
+
+		case *IfExpr:
+			recurse(ns, v.Condition)
+			recurse(ns, v.Then)
+			recurse(ns, v.Else)
+
+		case *WhileExpr:
+			recurse(ns, v.Condition)
+			recurse(ns, v.Do)
+
+		case *MatchExpr:
+			recurse(ns, v.Left)
+			recurse(ns, v.Cases)
+
+		case *Cases:
+			for _, c := range v.Cases {
+				recurse(ns, c)
+			}
+			if v.Null != nil {
+				recurse(ns, v.Null)
+			}
+
+		case *Case:
+			addNS(v, &v.Name)
+			recurse(ns, &v.Type)
+			recurse(ns, v.Body)
+
+		case *AssignExpr:
+			recurse(ns, &v.Left)
+			recurse(ns, v.Right)
+
+		case *VarExpr:
+			recurse(ns, v.Value)
+			addNS(v, &v.Name)
+			recurse(ns, &v.Type)
+			recurse(ns, v.Expr)
+
+		case *ChainExpr:
+			recurse(ns, v.Pre)
+			recurse(ns, v.Expr)
+
+		case NativeExpr:
+
+		default:
+			panic(v)
+		}
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+		}
+	}()
+
+	for _, c := range ast.Classes {
+		recurse(nil, c)
+	}
+
+	panic("unimplemented: typecheck")
 }
