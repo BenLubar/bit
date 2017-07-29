12d2a7cdf11f9d24135c5a0ba2effb330c00133d
Ben Lubar <ben.lubar@gmail.com>
2017-07-29 10:02:52 -0500

    document AssemblyWriter

--- cmd/bitc/gen.go
+++ cmd/bitc/gen.go
@@ -2,28 +2,61 @@ package main
 
 import "log"
 
+// AssemblyWriter is an interface that is used to write compiler output.
 type AssemblyWriter interface {
+	// Runtime writes any helper code that is shared between programs.
 	Runtime() error
+	// DataSegment informs the writer that the following function calls
+	// will define mutable data.
 	DataSegment() error
+	// TextSegment informs the writer that the following function calls
+	// will define executable code.
 	TextSegment() error
+	// DeclarePointer creates an address-of-a-bit variable.
 	DeclarePointer(n *Number) error
+	// Start marks the location where code will begin executing.
 	Start() error
+	// DeclareLine marks a line of BIT code.
 	DeclareLine(n *Number) error
+	// Goto jumps to a line depending on the value of the jump register.
+	// Jumping to a nil line number exits the program.
 	Goto(zero, one *Number) error
+	// Read reads one bit from the standard input into the jump register.
+	// If an error occurs, execution will resume on the line number given.
 	Read(eof *Number) error
+	// Print writes the bit in register 0 to the standard output.
 	Print() error
+	// SaveRegister pushes the register onto the stack.
 	SaveRegister(register int) error
+	// LoadRegister pops the register from the stack.
 	LoadRegister(register int) error
+	// PointerAddress puts the address of an address-of-a-bit variable into
+	// the register.
 	PointerAddress(register int, n *Number) error
+	// BitAddress puts the address of a bit variable into the register.
 	BitAddress(register int, n *Number) error
+	// JumpAddress puts the address of the jump register into the register.
 	JumpAddress(register int) error
+	// PointerValue puts the value of an address-of-a-bit variable into the
+	// register.
 	PointerValue(register int, n *Number) error
+	// ReadBit sets the dest register to the bit at the address in the src
+	// register. The registers may be the same.
 	ReadBit(dest, src int) error
+	// JumpValue puts the value of the jump register into the register.
 	JumpValue(register int) error
+	// BitValue puts a constant bit value into the register.
 	BitValue(register int, bit bool) error
+	// WritePointer puts the address-of-a-bit from src into the
+	// address-of-a-bit variable at the address in dest.
 	WritePointer(dest, src int) error
+	// WriteBit puts the bit from src into the bit variable at the address
+	// in dest.
 	WriteBit(dest, src int) error
+	// Advance increases the address of a bit variable in the register by
+	// the offset.
 	Advance(register, offset int) error
+	// NandBit sets dest to (src NAND dest).
 	NandBit(dest, src int) error
 }
 
